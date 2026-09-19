460945079cffe03d3df43c7a1c9b0262eaf21dce
Veerasivamani <veerasiva10500@gmail.com>
2026-09-19 14:13:29 +0530

    Stop tracking generated JS files

--- tests/Login.spec.js
+++
@@ -1,8 +0,0 @@
-import { test } from '../fixtures/fixtures.js';
-test('Login to the site', async ({ Loginpage, productpage }) => {
-    Loginpage.open();
-    Loginpage.Login('standard_user', 'secret_sauce');
-    // productpage.handleDialog()
-    // await expect(productpage.pageTitle).toHaveText('Products')
-});
-//# sourceMappingURL=Login.spec.js.map
